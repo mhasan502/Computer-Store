ba8dac1ac5375e22312083f1163228eb97e724ee
Mehedi Hasan <mhasan502@gmail.com>
2020-04-12 01:43:45 +0600

    indented codes

--- headfoot/footer.php
+++ headfoot/footer.php
@@ -4,54 +4,52 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
-    <link rel="stylesheet" href="./headfoot/bootstrap.min.css">
+    <link rel="stylesheet" href="./headfoot/custom.css">
     <link rel="stylesheet" href="./headfoot/footer.css">
 </head>
 
-<div class="content">
-</div>
-    <footer id="myFooter">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-3">
-                    <h2 class="logo"><a href="#"> LOGO </a></h2>
+<footer id="myFooter">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <h2 class="logo"><a href="#"> LOGO </a></h2>
+            </div>
+            <div class="col-sm-2">
+                <h5>Get started</h5>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sign up</a></li>
+                    <li><a href="#">Downloads</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-2">
+                <h5>About us</h5>
+                <ul>
+                    <li><a href="#">Company Information</a></li>
+                    <li><a href="#">Contact us</a></li>
+                    <li><a href="#">Reviews</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-2">
+                <h5>Support</h5>
+                <ul>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Help desk</a></li>
+                    <li><a href="#">Forums</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-3">
+                <div class="social-networks">
+                    <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+                    <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+                    <a href="#" class="google"><i class="fa fa-google-plus"></i></a>
                 </div>
-                <div class="col-sm-2">
-                    <h5>Get started</h5>
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Sign up</a></li>
-                        <li><a href="#">Downloads</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-2">
-                    <h5>About us</h5>
-                    <ul>
-                        <li><a href="#">Company Information</a></li>
-                        <li><a href="#">Contact us</a></li>
-                        <li><a href="#">Reviews</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-2">
-                    <h5>Support</h5>
-                    <ul>
-                        <li><a href="#">FAQ</a></li>
-                        <li><a href="#">Help desk</a></li>
-                        <li><a href="#">Forums</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-3">
-                    <div class="social-networks">
-                        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-                        <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-                        <a href="#" class="google"><i class="fa fa-google-plus"></i></a>
-                    </div>
-                    <button type="button" class="btn btn-default">Contact us</button>
-                </div>
+                <button type="button" class="btn btn-default">Contact us</button>
             </div>
         </div>
-        <div class="footer-copyright">
-            <p>© MD. Mehedi Hasan </p>
-        </div>
-    </footer>
+    </div>
+    <div class="footer-copyright">
+        <p>© MD. Mehedi Hasan </p>
+    </div>
+</footer>
 </html>
--- process/connect.php
+++ process/connect.php
@@ -1,11 +1,11 @@
-<?php /** @noinspection PhpRedundantClosingTagInspection */
+<?php
 
 $db_server = "localhost";
-    $db_user = "root";
-    $db_pass = "";
-    $db_name = "computer-store";
-    
-    $conn = mysqli_connect($db_server,$db_user,$db_pass,$db_name);
-    if(!$conn)
-        die("Connection Failed :".mysqli_connect_error());
+$db_user = "root";
+$db_pass = "";
+$db_name = "computer-store";
+
+$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+if (!$conn)
+    die("Connection Failed :" . mysqli_connect_error());
 ?>
